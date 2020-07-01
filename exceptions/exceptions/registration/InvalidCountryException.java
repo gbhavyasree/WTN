5a8b7f235b38cdd7a6f757d95cb99eda819b27ba
gbhavyasree <66791112+gbhavyasree@users.noreply.github.com>
2020-07-01 08:21:09 +0530

    registrtion

---
+++ exceptions/exceptions/registration/InvalidCountryException.java
@@ -0,0 +1,11 @@
+package com.wipro.exceptions;
+
+public class InvalidCountryException extends Exception {
+	public InvalidCountryException() 
+	{
+		super();
+		System.out.println("InvalidCountryException occured");
+		System.out.println("User Outside India  cannot be registered");
+	}
+
+}
---
+++ exceptions/exceptions/registration/UserRegistration.java
@@ -0,0 +1,33 @@
+package com.wipro.exceptions;
+
+class Registration
+{
+	void registerUser(String username,String userCountry)throws InvalidCountryException
+	{
+		if (!userCountry.equals("India"))
+		{
+			throw new InvalidCountryException();
+		}
+		else
+		{
+			System.out.println("User registration done successfully");
+		}
+	}
+}
+
+public class UserRegistration
+{
+	public static void main(String[] args)
+	{
+		Registration r1=new Registration();
+		try 
+		{
+			r1.registerUser("tinku", "US");
+		} 
+		catch (InvalidCountryException e) 
+		{
+			System.out.println(e.getMessage());
+		}
+		
+}
+}
